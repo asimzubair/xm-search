223815ea2eea5f5577942ebf79676be7b0ff6d51
asim_zubair <asim@nextgeni.com>
2022-07-07 14:01:51 +0500

    UL changes

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,6 +11,7 @@
 
                     @if (count($errors) > 0)
                         <div class="alert alert-danger">
+                            <ul></ul>
                             <ul>
                                 @foreach ($errors->all() as $error)
                                     <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
